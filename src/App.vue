0f30545ef42bbd28f144919ef28e6dff6230b400
iobox420 <67315235+iobox420@users.noreply.github.com>
2023-04-14 15:17:39 +0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app" class="container mt-4">
+    <label for="tags-basic">Type a new tag and press enter</label>
+    <b-form-tags input-id="tags-basic" v-model="value"></b-form-tags>
+    <p class="mt-2">Value: {{ value }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const value = ref<string[]>(['apple', 'orange']);
+</script>
